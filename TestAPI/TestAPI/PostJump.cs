a39151fbaf10b8f2b57d7608015721dd0b2580ce
Vera Tsaryk <vera.butouskaya@gmail.com>
2023-03-24 15:22:15 +0200

    JumpTestAPI

---
+++ TestAPI/TestAPI/PostJump.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APItestAutomation {
+   public class PostJump {
+       public string name { get; set; }
+        public int result { get; set;}
+        public int windSpeed { get; set; }
+        public int date { get; set; }
+        public bool isCompetition { get; set; }
+        public string personId { get; set; }
+       
+}
+}
---
+++ TestAPI/TestAPI/PutJumps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APItestAutomation {
+   public class PutJumps {
+        public string name { get; set; }
+        public int result { get; set; }
+        public double windSpeed { get; set; }
+        public int date { get; set; }
+        public bool isCompetition { get; set; }
+        public string personId { get; set; }
+        public string Id { get; set; }
+
+    }
+}
