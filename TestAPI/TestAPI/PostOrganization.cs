f011107581169a8ba503c6696039bb62e401c31f
Vera Tsaryk <vera.butouskaya@gmail.com>
2023-03-24 15:21:32 +0200

    OrganisationTestAPI

---
+++ TestAPI/TestAPI/PostOrganization.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APItestAutomation {
+   public class PostOrganization {
+        public string name { get; set; }
+    }
+}
---
+++ TestAPI/TestAPI/PutOrganization.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APItestAutomation {
+    public class PutOrganization {
+        public string name { get; set; }
+        public string Id { get; set; }
+    }
+}
